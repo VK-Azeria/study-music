f4e8a3097104047f9b1a54bf0bc6205b296abb81
Vladislav Klyukin <vk.azeria@gmail.com>
2025-11-08 22:36:13 +0500

    set up ci cd for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+    plugins: [
+        react({
+            babel: {
+                plugins: [["babel-plugin-react-compiler"]],
+            },
+        }),
+        viteSingleFile({
+            removeViteModuleLoader: true,
+            useRecommendedBuildConfig: true,
+        }),
+    ],
+    build: {
+        assetsInlineLimit: 100000000,
+        minify: "terser",
+        terserOptions: {
+            compress: {
+                drop_console: true,
+            },
+        },
+    },
+    base: "/study-music/",
+});
